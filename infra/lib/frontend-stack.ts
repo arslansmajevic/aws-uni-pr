1df707e5e4a240bfc60c84de66d287aaff59c586
Arslan Smajevic <iamarslanb@gmail.com>
2026-05-13 21:46:59 +0200

    feat: added frontend application and building logic for localstack

---
+++ infra/lib/frontend-stack.ts
@@ -0,0 +1,59 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as path from 'path';
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket for hosting the frontend
+    const frontendBucket = new s3.Bucket(this, 'FrontendBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    // Create a CloudFront distribution to serve the frontend
+    const distribution = new cloudfront.Distribution(this, 'FrontendDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(frontendBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(300),
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
+          ttl: cdk.Duration.seconds(300),
+        },
+      ],
+    });
+
+    // Deploy the frontend build output to S3
+    new s3deploy.BucketDeployment(this, 'DeployFrontend', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../apps/web/dist'))],
+      destinationBucket: frontendBucket,
+      distribution: distribution,
+      distributionPaths: ['/*'],
+    });
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'FrontendUrl', {
+      value: `https://${distribution.domainName}`,
+      description: 'URL of the frontend distribution',
+      exportName: 'FrontendUrl',
+    });
+  }
+}
